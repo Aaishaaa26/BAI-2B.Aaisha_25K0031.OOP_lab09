6b74ae685537cd9a8c65d1434bff60dfa61a8df8
Aaisha Munir <cakefunneh123@gmail.com>
2026-04-15 01:48:24 +0500

    Add files via upload

---
+++ Lab09/Codes/4.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+class PaymentMethod{
+	public:
+		virtual void processPayment(double amount) = 0;
+		virtual ~PaymentMethod(){}		
+};
+
+class CreditCard : public PaymentMethod{
+	string cardNumber;
+	public:
+		CreditCard(string num) : cardNumber(num) {}
+		
+		void processPayment(double amount) override{
+			cout << "_____Credit Card______" <<endl;
+			if(amount > 10000){
+				cout << "Payment Failed. (v large trans amount)" << endl;
+			}
+			else{
+				cout << "Card Declined" << endl;
+			}
+		}
+};
+
+class DigitalWallet : public PaymentMethod{
+	double balance;
+	public:
+		DigitalWallet(double bal) : balance(bal) {}
+		
+		void processPayment(double amount) override{
+			cout << "\n___Digital WAllet___" << endl;
+			if(amount > balance){
+				cout << "Payment Failed. (insuff amount, ghareeb)" << endl;
+			}
+			else{
+				balance -= amount;
+				cout << "Transaction Successful.\nRemaining Balnce: "<< balance << endl;
+			}
+		}
+};
+
+int main(){
+	PaymentMethod* p1 = new CreditCard("03980983");
+	PaymentMethod* p2 = new DigitalWallet(2000);
+	
+	p1->processPayment(500);
+	p2->processPayment(1850);
+}
